ce35ed2b36f23148b7935e0b7cc46119e4b87912
Aryan Soni <aryan.soni@btech.christuniversity.in>
2020-05-09 21:27:08 +0530

    modified .py file

--- ConvoNet/mnist-classifier/mnist-classifier.py
+++ ConvoNet/mnist-classifier/mnist-classifier.py
@@ -136,6 +136,7 @@ from keras.models import load_model
 classifier = load_model('./mnist-2.h5')
 
 #sudo apt-get install graphviz
+from keras.utils.vis_utils import plot_model
 
 plot_model(model,
                to_file='model.png',
